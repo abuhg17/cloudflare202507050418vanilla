7d268052385c18ca7e27a4aa8ea3d6d706e1b1ae
goldshoot0720 <goldshoot0720@gmail.com>
2025-06-28 20:10:55 +0800

    cloudflare202507050418vanilla

---
+++ git-cloudflare202507050418vanilla.bat
@@ -0,0 +1,12 @@
+git init
+
+git add .
+
+git commit -m "cloudflare202507050418vanilla"
+
+git remote add origin https://github.com/abuhg17/cloudflare202507050418vanilla.git
+
+git checkout -b main
+git push -u origin main --force
+
+pause
---
+++ open-cloudflare202507050418vanilla.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\chbon\Documents\cloudflare202507050418\cloudflare202507050418vanilla"
+explorer http://localhost:5173/
+npm run dev
